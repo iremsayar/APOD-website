eadab31ba8e662f60891fd6e424a047f09f04939
Irem <iremssayar@gmail.com>
2022-04-03 01:31:51 +0300

    fix: navbar

--- src/components/base/Navbar.jsx
+++ src/components/base/Navbar.jsx
@@ -1,36 +1,41 @@
-import React from 'react'
-import SearchProduct from '../pages/SearchProduct'
-import logo from '../assets/stars.png'
+import React from "react";
+import SearchProduct from "../pages/SearchProduct";
+import logo from "../assets/stars.png";
 import { Link } from "react-router-dom";
-import routes from '../../routes';
-import './Navbar.css'
+import routes from "../../routes";
+import "./Navbar.css";
 
 function Navbar() {
   return (
-
-    
     <nav className="container-fluid navbar navbar-expand-lg navbar-dark bg-dark border border-dark shadow-lg">
       <div className="col-sm-6 col-md-3">
-        
-        <div className="navbar-brand"> <Link to="/" className='text-decoration-none'><img src={logo} alt=" Logo" className='img-widht' />
-            <span className='nasa-text text-white'>APOD</span>
-            </Link>
-          </div>
-          
+        <div className="navbar-brand">
+          {" "}
+          <Link to="/" className="text-decoration-none">
+            <img src={logo} alt=" Logo" className="img-widht" />
+            <span className="nasa-text text-white">APOD</span>
+          </Link>
+        </div>
       </div>
 
       <div className="mobile-links d-md-none">
         <ul class="list-inline">
           <li class="list-inline-item">
-            {
-              routes.filter(item => item.isNav).map((item, index) =>
-                <Link key={index} className="text-light text-decoration-none px-2" to={item.path}>{item.title}</Link>)
-            }
+            {routes
+              .filter((item) => item.isNav)
+              .map((item, index) => (
+                <Link
+                  key={index}
+                  className="text-light text-decoration-none px-2"
+                  to={item.path}
+                >
+                  {item.title}
+                </Link>
+              ))}
           </li>
-
         </ul>
       </div>
-      
+
       <div className="col-sm-6 col-md-5">
         <div className="search-product">
           <SearchProduct />
@@ -40,15 +45,22 @@ function Navbar() {
       <div className=" col-md-3 ms-auto links d-none d-md-block">
         <div className="collapse navbar-collapse">
           <div className="navbar-nav">
-            {
-              routes.filter(item => item.isNav).map((item, index) =>
-                <Link key={index} className="nav-item nav-link h4 text-light mx-4" to={item.path}>{item.title}</Link>)
-            }
+            {routes
+              .filter((item) => item.isNav)
+              .map((item, index) => (
+                <Link
+                  key={index}
+                  className="nav-item nav-link h4 text-light mx-4"
+                  to={item.path}
+                >
+                  {item.title}
+                </Link>
+              ))}
           </div>
         </div>
-      </div> 
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,28 @@
-import Home from './components/pages/Home'
-import About from './components/pages/About'
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
 import Products from "./components/pages/Products";
-import ProductDetail from './components/pages/ProductDetail';
-import SearchProduct from './components/pages/SearchProduct';
-import SearchResult from './components/pages/SearchResult';
-import NotFound from './components/pages/NotFound';
+import ProductDetail from "./components/pages/ProductDetail";
+import SearchProduct from "./components/pages/SearchProduct";
+import SearchResult from "./components/pages/SearchResult";
+import NotFound from "./components/pages/NotFound";
 
 const routes = [
-    {title:"Home",path:"/",element:<Home/>, isNav:true},
-    {title:"About",path:"about",element:<About/>, isNav:true},
-    { title: "Pictures", path: "products", element: <Products />, isNav: true },
-    { title: "Product Detail", path: "products/:productId", element: <ProductDetail />, isNav: false },
-    { title: "Search", path: "searchProduct", element: <SearchResult />, isNav: false },
-    { title: "Not found", path: "*", element: <NotFound />, isNav: false },
-]
+  { title: "Home", path: "/", element: <Home />, isNav: true },
+  { title: "About", path: "about", element: <About />, isNav: true },
+  { title: "Pictures", path: "products", element: <Products />, isNav: false },
+  {
+    title: "Product Detail",
+    path: "products/:productId",
+    element: <ProductDetail />,
+    isNav: false,
+  },
+  {
+    title: "Search",
+    path: "searchProduct",
+    element: <SearchResult />,
+    isNav: false,
+  },
+  { title: "Not found", path: "*", element: <NotFound />, isNav: false },
+];
 
-export default routes
+export default routes;
